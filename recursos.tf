461e81ef2a7f4bdea985d1a4caae53b7b07883bc
Jose David Anacona <josedavid.anacona735@comunidadunir.net>
2025-07-26 15:03:54 -0500

    CP2 Fase 4 COMPLETA Despliegue AKS, ACR, volúmenes persistentes servicio LoadBalancer

--- recursos.tf
+++ recursos.tf
@@ -109,3 +109,37 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "cp2-aks"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "cp2aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin       = "azure"
+    dns_service_ip       = "10.0.0.10"
+    service_cidr         = "10.0.0.0/16"
+  }
+
+  tags = {
+    environment = "cp2"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
